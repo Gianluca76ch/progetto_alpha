06358c4058e09211eb86a4350fafd8f2c2c908c5
Gian <Gian@GIAN-PC>
2017-02-04 10:15:05 +0100

    Aggiungere i file di progetto.

---
+++ WebApplication1/xml_test.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="xml_test.aspx.vb" Inherits="WebApplication1.xml_test" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ WebApplication1/xml_test.aspx.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Xml
+Imports System.IO
+
+Public Class xml_test
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Dim xtw As XmlTextWriter
+        xtw = New XmlTextWriter("C:\Users\Gian\Desktop", Encoding.UTF8)
+        xtw.WriteStartDocument()
+        xtw.WriteStartElement("Customers")
+        xtw.WriteEndElement()
+        xtw.Close()
+
+    End Sub
+
+End Class
